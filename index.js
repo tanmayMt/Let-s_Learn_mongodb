714e17101bd262733bdc4a1f041db63e13918211
web-collab <tanmoy587@gmail.com>
2025-01-22 20:50:21 +0530

    Create a /prducts post routes

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ const mongoose = require("mongoose");
 const port = 3002;
 const app = express();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/mongodb_learn");
-    console.log("mongodb is connected");
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 // Mongoose schema & model
 // - define structure of document with validation
@@ -43,6 +35,33 @@ const productSchema = new mongoose.Schema({
 });
 
 const Product = mongoose.model("Products", productSchema);
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/mongodb_learn");
+    console.log("mongodb is connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome To Products Page</h1>");
+});
+
+app.post("/products",(req,res)=>{
+  
+    const title = req.body.title;
+    const price = req.body.price;
+    const description = req.body.description;
+    res.status(201).send({title,price,description});
+  
+
+});
 
 app.listen(port, async () => {
   console.log(`server is running at http://localhost:${port}`);
